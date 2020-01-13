51171320d7af8abe7970bac7cb18e06daf2fd008
胡加偉 <45283397+erichu30@users.noreply.github.com>
2020-01-13 23:03:22 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char s;
+    int flag = 0;
+
+    while(cin.get(s)){
+        if(s == '"'){
+            if(flag %2 == 0){
+                cout << "``";
+            }else{
+                cout << "''";
+            }
+            flag++;
+        }else{
+            cout << s;
+        }
+
+    }
+    return 0;
+}
